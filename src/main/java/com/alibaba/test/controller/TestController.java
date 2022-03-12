5fbba10a1058c764c169440138ab6b6553e81e4c
qweqe417 <1820949623@qq.com>
2022-03-12 15:36:25 +0800

    test

---
+++ src/main/java/com/alibaba/test/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.alibaba.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/getName")
+     public String getName() {
+         return "Success";
+     }
+}
---
+++ src/test/java/com/alibaba/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
